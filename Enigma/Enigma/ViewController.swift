4269543604fabc082f64277488b1f587c6cb8479
hbc <bcxxxxxx@gmail.com>
2016-02-23 23:51:45 +0800

    chore: init iOS workspace.

---
+++ Enigma/Enigma/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Enigma
+//
+//  Created by hbc on 2/23/16.
+//  Copyright © 2016 hbc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
